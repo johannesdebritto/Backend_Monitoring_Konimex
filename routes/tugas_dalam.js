b4272adb192b6192ae1feb0d609988669fbc87d0
johannes debritto <johanesdebrito22@gmail.com>
2025-03-20 14:26:59 +0700

    commit

--- routes/tugas_dalam.js
+++ routes/tugas_dalam.js
@@ -167,7 +167,6 @@ router.get('/patroli-dalam/jumlah/:id_riwayat', async(req, res) => {
     }
 });
 
-
 router.get('/riwayat/:id_riwayat', async(req, res) => {
     console.log("GET /api/riwayat/:id_riwayat diakses!");
     const { id_riwayat } = req.params;
@@ -178,8 +177,8 @@ router.get('/riwayat/:id_riwayat', async(req, res) => {
 
     try {
         const query = `
-            SELECT bagian, keterangan_masalah, tanggal 
-            FROM riwayat 
+            SELECT bagian, keterangan_masalah, waktu, tanggal 
+            FROM detail_riwayat_dalam 
             WHERE id_riwayat = ?
         `;
         const [rows] = await db.execute(query, [id_riwayat]);
@@ -188,12 +187,12 @@ router.get('/riwayat/:id_riwayat', async(req, res) => {
             return res.status(404).json({ message: 'Data tidak ditemukan' });
         }
 
-        return res.json(rows[0]);
-
+        return res.json(rows); // ✅ Balikin semua data (array)
     } catch (err) {
         console.error('❌ Database error:', err);
         return res.status(500).json({ message: 'Terjadi kesalahan server' });
     }
 });
+
 
 module.exports = router;
