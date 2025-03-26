414d24064ec9ad2f5faee7ab27edffe67725a9f4
johannes debritto <johanesdebrito22@gmail.com>
2025-03-26 13:58:31 +0700

    commit

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const tugasRoutes = require('./routes/tugas');
 const submitRoutes = require('./routes/submit');
 const statusRoutes = require('./routes/status');
 const tugasdalamRoutes = require('./routes/tugas_dalam');
-const historyRoutes = require('./routes/history');
+const historyRoutes = require('./routes/history'); // Pastikan sudah di-import
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -33,7 +35,7 @@ app.use('/api/tugas', tugasRoutes);
 app.use('/api/tugas_dalam', tugasdalamRoutes);
 app.use('/api/submit', submitRoutes);
 app.use('/api/status', statusRoutes);
-app.use('/api/history', historyRoutes);
+app.use('/api/history', historyRoutes); // Pastikan route sudah dikonfigurasi
 
 // Jalankan server
 app.listen(PORT, "0.0.0.0", () => {
